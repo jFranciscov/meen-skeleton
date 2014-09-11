3f7be7a4649a001f890d2df962e834fff935b534
jfranciscov <crusierr@gmail.com>
2014-09-11 12:30:05 -0300

    prueba gitignore

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var fs        = require('fs')
   , path      = require('path')
   , Sequelize = require('sequelize')
   , lodash    = require('lodash')
-  , sequelize = new Sequelize('a', 'a', 'a', {
+  , sequelize = new Sequelize('b', 'b', 'b', {
       dialect: "mysql", 
       port:    3306, // 5432 (postgres)
       host:   "localhost"
