aa4b28205e53edc70eacccfdcada07adb7acc250
jfranciscov <crusierr@gmail.com>
2014-09-12 17:08:55 -0300

    Compresion archivos css y js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var wrap = require('gulp-wrap');
 var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var declare = require('gulp-declare');
 var handlebars = require('gulp-handlebars');
@@ -19,10 +20,20 @@ gulp.task('templates', function() {
     .pipe(gulp.dest('public/js/'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('js', function() {
     return gulp.src(['public/js/**/*.js','!public/js/vendor/*.js','!public/js/templates.js'])
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('css', function () {
+    return gulp.src('public/css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default',['js','css']);
